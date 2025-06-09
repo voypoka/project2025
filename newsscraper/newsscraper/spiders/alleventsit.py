9ffa2931cb14574afb98fa6d85432a84b3edfdf5
Павел Старостин <pavelstarostin@MacBook-Air-Pavel.local>
2025-06-09 10:27:18 +0300

    Added Itc2Go, AllEvents without description and Itc2Go has errors

--- newsscraper/newsscraper/spiders/alleventsit.py
+++ newsscraper/newsscraper/spiders/alleventsit.py
@@ -2,35 +2,56 @@ import scrapy
 import re
 
 class AllEventsIT(scrapy.Spider):
-    name = "itc2go"
+    name = "alleventsit"
     start_urls = [
         "https://all-events.ru/events/calendar/city-is-nizhniy_novgorod/"
     ]
 
     def get_title(self, info):
-        return ""
-    
-    def get_date(self, info):
-        return ""
+        return info.css("div.event_flex_content").css("div.event_order_1").css("a.event_name_new::text").get()
+
+    def get_start_date(self, info):
+        result = info.xpath('.//div[@class="event-date"]/div[not(@class)]/text()').get()
+        return result[0:10]
+
+    def get_end_date(self, info):
+        text = info.xpath('.//div[@class="event-date"]/div[not(@class)]/text()').get()
+        text = text.replace('\xa0', ' ').strip()
+        match = re.search(r'(\d{2}\.\d{2}\.\d{4})(?:\s*-\s*(\d{2}\.\d{2}\.\d{4}))?', text)
+        if match:
+            if match.group(2):
+                return match.group(2)
+            return match.group(1)
+
+        return None
     
     def get_location(self, info):
-        return ""
+        return ','.join(info.css(
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_hybrid span:nth-of-type(2)::text,"
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_ span:nth-of-type(2)::text,"
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_offline span:nth-of-type(2)::text"
+        ).getall())
     
     def get_description(self, info):
         return ""
     
     def get_tags(self, info):
-        return ""
-            
-    
+        tags_div = info.css("div.event_flex_content div.event_width.order_2 div.teg_content").get()
+        # Получаем все ссылки (теги) из div
+        tags = info.css("div.teg_content a::text").getall()
+        return ', '.join(tag.strip() for tag in tags)  # Объединяем их в строку через запятую
+
     def parse(self, response):
-        for info in response.css("div.event.panel.panel-default"): # вот тут нужно думать
+
+        for info in response.css("div.event_flex_item"):
             yield {
                 "name": self.get_title(info),
-                "start": self.get_date(info),
-                "end": self.get_date(info),
+                "start": self.get_start_date(info),
+                "end": self.get_end_date(info),
                 "location": self.get_location(info),
                 "description": self.get_description(info),
                 "tags": self.get_tags(info)
             }
-
--- newsscraper/newsscraper/spiders/itc2go.py
+++ newsscraper/newsscraper/spiders/itc2go.py
@@ -8,29 +8,35 @@ class Itc2Go(scrapy.Spider):
     ]
 
     def get_title(self, info):
-        return ""
+        return info.css("div.index-events div#index_events div.index-events-item.media div.media-body a.event-title::text").get()
     
-    def get_date(self, info):
-        return ""
+    def get_start_date(self, info):
+        return info.css("div.index-events div#index_events div.index-events-item.media div.media-body div.date-place").get()
+
+
+    def get_end_date(self, info):
+        return None
     
-    def get_location(self, info):
+    def get_location(self, info): #надо переходить по ссылке
         return ""
     
     def get_description(self, info):
         return ""
     
     def get_tags(self, info):
-        return ""
+        tags_div = info.css("div.index-events div#index_events div.index-events-item.media div.media-body div.event-themes").get()
+        # Получаем все ссылки (теги) из div
+        tags = info.css("div.event-themes a::text").getall()
+        return ', '.join(tag.strip() for tag in tags)  # Объединяем их в строку через запятую
             
     
     def parse(self, response):
-        for info in response.css("div.event.panel.panel-default"): # вот тут нужно думать
+        for info in response.css("div.index-events div#index_events div.index-events-item.media"):
             yield {
                 "name": self.get_title(info),
-                "start": self.get_date(info),
-                "end": self.get_date(info),
+                "start": self.get_start_date(info),
+                "end": self.get_end_date(info),
                 "location": self.get_location(info),
                 "description": self.get_description(info),
                 "tags": self.get_tags(info)
             }
-
